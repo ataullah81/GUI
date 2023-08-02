0e60b8d83b1a0b4fb36e2098ad88dd5cc0a41079
Ataullah Behesti <ataullah.behesti@gmail.com>
2023-08-02 20:46:51 +0300

    Tkinter tutorial

--- Tkinter/using icons images and exit button.py
+++ Tkinter/using icons images and exit button.py
@@ -1,10 +1,21 @@
 from tkinter import *
+
+from PIL import ImageTk,Image
+
 root = Tk()
 
 root.title("Learn python")
 #root.iconbitmap('logo.png')
-
 img = PhotoImage(file='icon.png')
 root.iconphoto(False, img)
+
+my_img = ImageTk.PhotoImage(Image.open('apple.png'))
+my_label = Label(image=my_img) # Same way we can put image on the button
+my_label.pack()
+
+
+
+btn = Button(root, text='Exit',font=('Helvetica',20),command=root.quit)
+btn.pack()
 
 root.mainloop()
