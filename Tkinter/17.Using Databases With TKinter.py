bc7af3ad4020343f6ba3437dcbf684ac31b75deb
Ataullah Behesti <ataullah.behesti@gmail.com>
2023-08-16 17:44:41 +0300

    using database with tkinter

--- Tkinter/17.Using Databases With TKinter.py
+++ Tkinter/17.Using Databases With TKinter.py
@@ -5,7 +5,7 @@ import  sqlite3
 
 root = Tk()
 root.title('Database Connection')
-root.geometry('500x500')
+root.geometry('300x500')
 img = PhotoImage(file='icon.png')
 root.iconphoto(False, img)
 
@@ -27,6 +27,19 @@ cur_sor.execute("""CREATE TABLE addresses(
                 zipcode integer)
 """)
 '''
+# Create function to delete a record
+def delete():
+    conn = sqlite3.connect('address_book.db')
+
+    # Create cursor
+    cur_sor = conn.cursor()
+#Delete a record
+    cur_sor.execute('DELETE from addresses WHERE oid =' +  delete_box.get())
+
+    # Commit changes
+    conn.commit()
+    # Close connection
+    conn.close()
 #create submit function for database
 def submit():
     conn = sqlite3.connect('address_book.db')
@@ -76,9 +89,9 @@ def show():
 # Loop through results
     print_records = ''
     for record in records:
-        print_records += str(record[0]) + " " + str(record[1]) +'\n'
+        print_records += str(record[0]) + " " + str(record[1]) + " " + "\t" + str(record[6]) + '\n'
     query_lbl = Label(root, text = print_records)
-    query_lbl.grid(row=8,column=0, columnspan=2)
+    query_lbl.grid(row=11,column=0, columnspan=2)
 
     # Commit changes
     conn.commit()
@@ -88,7 +101,7 @@ def show():
 
 # Create text boxes
 first_name = Entry(root,width=30)
-first_name.grid(row=0,column=1, padx=20)
+first_name.grid(row=0,column=1,pady=(10,0), padx=20)
 
 last_name = Entry(root,width=30)
 last_name.grid(row=1,column=1, padx=20)
@@ -105,9 +118,12 @@ state.grid(row=4,column=1, padx=20)
 zipcode = Entry(root,width=30)
 zipcode.grid(row=5,column=1, padx=20)
 
+delete_box = Entry(root,width=30)
+delete_box.grid(row=9, column= 1)
+
 # Create labels
 first_name_lbl = Label(root,text ='First Name: ')
-first_name_lbl.grid(row=0,column=0)
+first_name_lbl.grid(row=0,column=0 )
 
 last_name_lbl = Label(root,text ='Last Name: ')
 last_name_lbl.grid(row=1,column=0)
@@ -121,9 +137,12 @@ city_lbl.grid(row=3,column=0)
 state_lbl = Label(root,text ='State: ')
 state_lbl.grid(row=4,column=0)
 
+
 zipcode_lbl = Label(root,text ='Post code: ')
 zipcode_lbl.grid(row=5,column=0)
 
+delete_lbl = Label(root,text='Delete ID: ')
+delete_lbl.grid(row=9,column=0)
 
 # create submit button
 btn = Button(root,text='Add record',command=submit)
@@ -131,7 +150,11 @@ btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 # create query button
 btn_query = Button(root, text ='Show records', command=show)
-btn_query.grid(row=7, column=0,columnspan=2,pady=10, padx=10, ipadx=100)
+btn_query.grid(row=7, column=0,columnspan=2,pady=10, padx=10, ipadx=95)
+#Create a delete button
+
+btn_delete = Button(root,text='Delete Record', command=delete)
+btn_delete.grid(row=10,column=0,columnspan=2,pady=10, padx=10, ipadx=95)
 
 # Commit changes
 conn.commit()
